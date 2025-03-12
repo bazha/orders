3b423310dc8e7517c3f13ce80f610ad58bd3b55d
Artur Bazhanau <a.bazhanov@dialmyapp.com>
2025-03-12 23:52:28 +0200

    add service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, EventPattern } from '@nestjs/microservices';
 
 @Controller('orders')
 export class AppController {
-  @MessagePattern('getOrders')
+  @MessagePattern('get_orders')
   getOrders(data: string): string {
     return data;
+  }
+
+  @EventPattern('create_order')
+  createOrder(data) {
+    console.log('data received, saving in db', data);
   }
 }
